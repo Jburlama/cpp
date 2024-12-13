fb5244acdc9c245107addbcf282a982e6d1dcc2b
jhonas <jhonas.burlamaqui@gmail.com>
2024-12-13 08:43:53 +0000

    ex04

---
+++ cpp01/ex04/src/main.cpp
@@ -0,0 +1,39 @@
+#include "../header/sed.hpp"
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		std::cout << "Usage: <file> s1 s2" << std::endl;
+		return (0);
+	}
+	std::string		outfile_name;
+	std::string		line;
+	std::ofstream	outfile;
+	std::ifstream	infile(argv[1]);
+
+	if (infile.is_open())
+	{
+		outfile_name = argv[1];
+		outfile_name.append(".replace");
+		outfile.open(outfile_name.c_str());
+		
+		size_t poss;
+		while (getline(infile, line))
+		{
+			while (42)
+			{
+				poss = line.find(argv[2]);
+				if (poss == std::string::npos)
+					break ;
+				line.erase(poss, strlen(argv[2]));
+				line.insert(poss, argv[3]);
+			}
+			outfile << line << std::endl;
+		}
+		infile.close();
+		outfile.close();
+	}
+	else
+		std::cout << "Could not open file!" << std::endl;
+}
